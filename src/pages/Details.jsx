5c57733a42495aa5d0122cb13f8999e8c36d53d1
Muhammed Naseef <muhammed.naseef@grit-stone.com>
2022-11-07 09:37:07 +0530

    use navaigate and useLocation

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,49 +1,43 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
-import './details.css'
-import { useNavigate, useParams, useLocation } from 'react-router-dom'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import "./details.css";
+import { useNavigate, useParams, useLocation } from "react-router-dom";
 
 export default function Details() {
+  const location = useLocation();
+  const state = location.state || {};
+  const { userId } = useParams();
+  const [user, setuser] = useState(state.data);
 
-const location = useLocation()
-const state = location.state || {}
-const {userId} = useParams()
-const [user, setuser] = useState(state.data)
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!user) {
+      fetchApi();
+    }
+  }, [userId, user]);
+
+  const fetchApi = async () => {
+    const results = await axios.get(
+      "https://jsonplaceholder.typicode.com/users/" + userId
+    );
+    setuser(results?.data);
+  };
 
 
-const navigate = useNavigate()
-
-
-useEffect(() => {
-  if(!user){
-
-    fetchApi();
-  }
-}, [userId,user]);
-
-const fetchApi = async () => {
-  const results = await axios.get(
-    "https://jsonplaceholder.typicode.com/users/"+userId
-  );
-  setuser(results?.data);
-};
-
-// console.log(data);
-    return (
-    <div className='page'>
-       <div className="title">
-            {user?.name}
-       </div>
-       <div className="body">
-            {user?.username}
-       </div>
-       <div className="body">
-            {user?.email}
-        </div>
-        <div className="body">
-        
-        </div>
-        <button onClick={()=>{navigate(-1)}}>go back</button>
+  return (
+    <div className="page">
+      <div className="title">{user?.name}</div>
+      <div className="body">{user?.username}</div>
+      <div className="body">{user?.email}</div>
+      <div className="body"></div>
+      <button
+        onClick={() => {
+          navigate(-1);
+        }}
+      >
+        go back
+      </button>
     </div>
-  )
+  );
 }
